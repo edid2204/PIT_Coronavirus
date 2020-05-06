a7f49e94572349c5373e96d108f163bed8bd67b3
= <edid2204@hotmail.com>
2020-05-06 18:46:28 -0500

    Primer commit

---
+++ PIT/src/Vista/Frm_Cliente.java
@@ -0,0 +1,12 @@
+package Vista;
+
+public class Frm_Cliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Cliente 0001");
+
+	}
+
+}
